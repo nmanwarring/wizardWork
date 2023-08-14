8f6e2f925bd650dfac7119d7edd13d1a3804f026
Andrew Cline <acline@precisionplanting.com>
2023-08-13 21:08:33 -0500

    feat: corrected content files

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-const http = require("http");
+const express = require("express");
 
-const hostname = "127.0.0.1";
-const port = 3000;
+es6Renderer = require("express-es6-template-engine");
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("This is Nate's Server.  Deal with it!!");
+app = express();
+
+app.engine("html", es6Renderer);
+app.set("view engine", "html");
+app.get("/", (req, res) => {
+  res.render("home", {
+    locals: { header1: "RPG's" },
+    partials: { title: "RPG's vs FPS" },
+  });
 });
-
-server.listen(port, hostname, () => {
-  console.log(`Server runnning at http://${hostname}:${port}/`);
-});
+// app.get("/", (req, res) => {
+//   res.render("linkedPage", {
+//     locals: { header2: "FPS" },
+//     partials: { title: "RPG's VS FPS" },
+//   });
+// });
+app.listen(3000);
+console.log();
